032036f89f114ea8cebb7a7c79c18eb61c954870
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-08-09 15:14:39 -0400

    passing tests

---
+++ index.js
@@ -0,0 +1,15 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+let increment = n => {
+    n++
+    return n
+}
+let decrement = n => {
+    n--
+    return n
+}
+
+let makeInt = (n) => parseInt(n, 10);
+let preserveDecimal = n => parseFloat(n);
